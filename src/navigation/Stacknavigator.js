16ebed0850c775ecc1356a92d4895de28e221b75
Umeraftab7747 <umeraftab7747@gmail.com>
2020-09-27 14:31:53 -0700

    Done

--- src/navigation/Stacknavigator.js
+++ src/navigation/Stacknavigator.js
@@ -32,6 +32,11 @@ export default class Stacknavigator extends Component {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen
+            name="Dashboard"
+            component={Dashboard}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
             name="SplashScreen"
             component={SplashScreen}
             options={{headerShown: false}}
@@ -39,11 +44,6 @@ export default class Stacknavigator extends Component {
           <Stack.Screen
             name="EmpDashboard"
             component={EmpDashboard}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Dashboard"
-            component={Dashboard}
             options={{headerShown: false}}
           />
 
